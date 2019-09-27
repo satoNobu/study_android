12d352c131586aadeeb8f44ed192c24724cbdf2d
satoNobu <nananiku.s.60@gmail.com>
2019-09-27 11:34:04 +0900

    最低限のアノテーション処理を追加

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById(R.id.textView)
+    TextView text;
+
+    @AfterViews
+    void Initialize() {
+        // 初期化処理など
+        text.setText("アノテーション");
+
     }
+
 }
